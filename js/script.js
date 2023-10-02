e6c7bbc08fc7fe0310ec57868f475f7b01a92e1c
Yilmar A. Vega G <31775736+yilmarvegag@users.noreply.github.com>
2023-10-02 01:53:13 -0500

    First commit and deploy to github pages
    Project pending any sliders

---
+++ js/script.js
@@ -0,0 +1,55 @@
+const slides = document.querySelectorAll('.slide');
+const nextButton = document.querySelector('#nextButton');
+const prevButton = document.querySelector('#prevButton'); // Agrega el botón "Atrás"
+
+let currentSlideIndex = 0;
+
+function showSlide(index) {
+    slides.forEach((slide, i) => {
+        if (i === index) {
+            slide.style.display = 'block';
+        } else {
+            slide.style.display = 'none';
+        }
+    });
+}
+
+function updateNavigation() {
+    if (currentSlideIndex === slides.length - 1) {
+        nextButton.style.display = 'none';
+    } else {
+        nextButton.style.display = 'block';
+    }
+    if (currentSlideIndex === 0) {
+        prevButton.style.display = 'none'; // Ocultar el botón "Atrás" en el primer slide
+    } else {
+        prevButton.style.display = 'block'; // Mostrar el botón "Atrás" en otros slides
+    }
+}
+
+function goToNextSlide() {
+    if (currentSlideIndex < slides.length - 1) {
+        currentSlideIndex++;
+        showSlide(currentSlideIndex);
+        updateNavigation();
+    }
+}
+
+function goToPrevSlide() { // Función para retroceder al slide anterior
+    if (currentSlideIndex > 0) {
+        currentSlideIndex--;
+        showSlide(currentSlideIndex);
+        updateNavigation();
+    }
+}
+
+showSlide(currentSlideIndex);
+updateNavigation();
+
+nextButton.addEventListener('click', () => {
+    goToNextSlide();
+});
+
+prevButton.addEventListener('click', () => { // Agregar evento para el botón "Atrás"
+    goToPrevSlide();
+});
